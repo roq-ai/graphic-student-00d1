8046b390cc1fcd07d99f969be1148ba4f156190d
roq-ai <>
2023-07-24 18:28:15 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,16 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Administrator'],
+  customerRoles: [],
+  tenantRoles: ['Administrator', 'Teacher'],
+  tenantName: 'School',
+  applicationName: 'GRAPHIC STUDENT MANAGEMENT',
+  addOns: ['file'],
+};
